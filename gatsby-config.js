072b89857ee3c3b5155c1edbd9daf21c17396619
Shreyas Patil <shreyaspatilg@gmail.com>
2020-06-17 15:25:15 +0530

    Update prism and gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,7 +24,7 @@ module.exports = {
             classPrefix: "language-",
             inlineCodeMarker: null,
             aliases: {},
-            showLineNumbers: false,
+            showLineNumbers: true,
             noInlineHighlight: false,
           },
         },
